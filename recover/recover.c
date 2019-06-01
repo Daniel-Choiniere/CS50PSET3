bf57705b84849bcfcac6a655ddbc3d0574ce04d9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-06-01 20:07:21 +0000

    mocked up pseudocode to work through problem

--- recover/recover.c
+++ recover/recover.c
@@ -3,10 +3,18 @@
 #include <stdlib.h>
 
 // open the memory card file
+// repeat until end of the card
+    // read 512 bytes at a time
     // find the beginning of JPEG
-    // open a new JPEG
-        // write 512 bytes until the new JPEG is found
-        // detect the end of the file
+        // yes -->
+            // open a new JPEG
+            // write 512 bytes until the new JPEG is found
+            // detect the end of the file
+        // no -->
+    // already found a JPEG
+        // yes -->
+        // no -->
+// close any remaing files
 
 int main(int argc, char *argv[])
 {
@@ -37,8 +45,17 @@ int main(int argc, char *argv[])
     }
 
 
-    // open the new JPEG file
-    sprintf(fiename, "%00.jpg", 2);
-    // write the new JPEG FILE
-    fwrite(data, size, number, outptr);
+
+    // // create a filename for the new JPEG file
+    // sprintf(fiename, "%00.jpg", 2);
+
+    // // open the new JPEG file so we can work with it
+    // ex) FILE *img = fopen(filename, "w");
+
+    // // now that the new file is open we need to write each JPEG 512 bytes at a time
+    // fwrite(data, size, number, outptr);
+    // ex) fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr) *outptr is the file we just created
+
+
+
 }
