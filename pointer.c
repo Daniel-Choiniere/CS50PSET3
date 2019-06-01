ade658f9ddebc26e5898986f9bfc7ce43812d0f3
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-06-01 16:30:53 +0000

    fixed bug with struct not being defined

--- pointer.c
+++ pointer.c
@@ -4,12 +4,13 @@
 // struct called node containing an int identified as 'value',
 // refernece to another node identfied as 'next'
 
-
 typedef struct
 {
     int VALUE;
-    node *next;
-} node;
+    struct NODE *next;
+} NODE;
+
+
 
 int add(int *x, int y)
 {
@@ -19,8 +20,12 @@ int add(int *x, int y)
 
 int main(void)
 {
-    int a = 5;
-    int b = 10;
-    printf("the total is: %i\n", add(&a, b));
-    printf("the total is: %i\n", add(&a, b));
+    // int a = 5;
+    // int b = 10;
+    // printf("the total is: %i\n", add(&a, b));
+    // printf("the total is: %i\n", add(&a, b));
+
+    NODE rootNode;
+    rootNode.VALUE = 5;
+    printf("%i\n", rootNode.VALUE);
 }
