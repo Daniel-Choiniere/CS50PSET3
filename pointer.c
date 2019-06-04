34c82138815ba65ebdb938e1c1727c7dcf4f07ed
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-06-04 01:41:58 +0000

    after class problem instructions

--- pointer.c
+++ pointer.c
@@ -9,34 +9,34 @@ typedef struct NODE
     struct NODE *next;
 } NODE;
 
+NODE *rootNode;
 
-// iterate through the linked list until you get to the end
-
-void addNode(int value, NODE *homeNode)
+void addNode(int value)
 {
-    NODE *trav = homeNode;
-    while (true)
-        {
-            if (trav->next == NULL)
-        {
-            NODE *newNode = malloc(sizeof(NODE));
-            newNode->value = value;
-            newNode->next = NULL;
-            trav->next = newNode;
-            break;
-        } else
-        {
-            trav = trav->next;
-            // printf("Moved Deeper\n");
-        }
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    newNode->next = NULL;
+
+    if (rootNode == NULL)
+    {
+        rootNode = newNode;
+        return;
     }
+
+    NODE *trav = rootNode;
+
+    while (trav->next == NULL)
+    {
+        trav = trav->next;
+    }
+
 }
 
 
     // iterate through the list and print each number until you hit the end i.e. 5, 10, 8, 17)
-void displayList(NODE *trav)
-
+void displayList()
 {
+    NODE *trav;
     while (trav != NULL)
     {
         printf("%d", trav->value);
@@ -49,25 +49,25 @@ void displayList(NODE *trav)
             printf(", ");
         }
     }
-
     printf("\n");
 }
 
 int main(void)
 {
-    NODE rootNode;
-    rootNode.value = 5;
-    rootNode.next = NULL;
-
-    addNode(10, &rootNode);
-    addNode(8, &rootNode);
-    addNode(17, &rootNode);
+    addNode(5);
+    addNode(10);
+    addNode(8);
+    addNode(17);
+    displayList();
 
     // printf("%i\n", rootNode.value);
     // printf("%i\n", rootNode.next->value);
     // printf("%i\n", rootNode.next->next->value);
     // printf("%i\n", rootNode.next->next->next->value);
 
-    // GOAL IS TO GET THE NUMBERS TO BE DISPLAYED ONE BY ONE
-    displayList(&rootNode);
+    // GOAL IS TO be able to clear out all the items in the list automatically
+        // each item needs to be removed individually, starting from the last value in the list
+        // find the one beofre the last free him and make him NULL
+
+
 }
